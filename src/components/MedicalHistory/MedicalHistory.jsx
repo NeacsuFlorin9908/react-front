e1557c5423f2638874febab764f67ca94aea1175
Mihai Chiciu <mihai.chiciu@jurata.ch>
2022-05-29 18:12:15 +0300

    added DoughnutChart

--- src/components/MedicalHistory/MedicalHistory.jsx
+++ src/components/MedicalHistory/MedicalHistory.jsx
@@ -1,8 +1,10 @@
 import React, { useEffect, useState } from 'react';
 import { useChatContext } from 'stream-chat-react';
+import DonutChart from 'react-donut-chart';
 
 const MedicalHistory = () => {
   const [users, setUsers] = useState([]);
+  const [myUsers, setMyUsers] = useState([]);
   const [selectedUser, setSelectedUser] = useState(undefined);
   const [selectValue, setSelectValue] = useState(undefined);
   const { client } = useChatContext();
@@ -41,43 +43,73 @@ const MedicalHistory = () => {
     }
   }, [client]);
 
+  useEffect(() => {
+    if (users) {
+      const myNewUsers = users.reduce((result, user) => {
+        const history = user.medicalHistory.filter((recipe) => recipe.medicName === client.user.name);
+        if (history.length) result.push({ ...user, medicalHistory: history });
+        return result;
+      }, []);
+      setMyUsers(myNewUsers);
+    }
+  }, [users]);
+
   return (
     <div className='history__container'>
-      {client.user.isMedic === 'true' && (
-        <>
-          <select className='history__select' onChange={handleChange}>
-            <option value='' disabled selected>
-              Selectează pacient
-            </option>
-            {users.map((user) => (
-              <option key={user.name} value={user.name}>
-                {user.fullName}
+      <div className='history__wrapper'>
+        {client.user.isMedic === 'true' && (
+          <>
+            <select className='history__select' onChange={handleChange}>
+              <option value='' disabled selected>
+                Selectează pacient
               </option>
+              {users.map((user) => (
+                <option key={user.name} value={user.name}>
+                  {user.fullName}
+                </option>
+              ))}
+            </select>
+            <button className='history__button' onClick={() => handleClick()}>
+              Verifică istoric
+            </button>
+          </>
+        )}
+        {selectedUser && (
+          <div>
+            {selectedUser.medicalHistory ? (
+              <h3 className='history__header'>Istoricul medical pentru {selectedUser.fullName}:</h3>
+            ) : (
+              <h3 className='history__header'>Nu există istoric pentru {selectedUser.fullName}</h3>
+            )}
+            {(selectedUser.medicalHistory || []).map((item) => (
+              <div className='history__token'>
+                <p>Control realizat de {item.medicFullName}</p>
+                <p>Diagnostic</p>
+                <p>{item.diagnosis}</p>
+                <p>Observații</p>
+                <p>{item.observations}</p>
+                <p>Tratament</p>
+                <p>{item.treatment}</p>
+              </div>
             ))}
-          </select>
-          <button className='history__button' onClick={() => handleClick()}>Verifică istoric</button>
-        </>
-      )}
-      {selectedUser && (
-        <div>
-          {selectedUser.medicalHistory ? (
-            <h3 className='history__header'>Istoricul medical pentru {selectedUser.fullName}:</h3>
-          ) : (
-            <h3 className='history__header'>Nu există istoric pentru {selectedUser.fullName}</h3>
-          )}
-          {(selectedUser.medicalHistory || []).map((item) => (
-            <div className='history__token'>
-              <p>Control realizat de {item.medicFullName}</p>
-              <p>Diagnostic</p>
-              <p>{item.diagnosis}</p>
-              <p>Observații</p>
-              <p>{item.observations}</p>
-              <p>Tratament</p>
-              <p>{item.treatment}</p>
-            </div>
-          ))}
-        </div>
-      )}
+          </div>
+        )}
+      </div>
+      <div>
+        {myUsers.length && (
+          <>
+            <h3 className='history__header'>Retetele scrise de catre {client.user.fullName}</h3>
+            <DonutChart
+              width={400}
+              innerRadius={0.5}
+              data={myUsers.map((user) => ({
+                label: user.fullName,
+                value: user.medicalHistory.length,
+              }))}
+            />
+          </>
+        )}
+      </div>
     </div>
   );
 };
